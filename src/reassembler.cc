bb83f8b78de929bc7141cc0f5c95d059f7eca3dd
ursocool <jaysecret@foxmail.com>
2024-07-04 00:53:25 +0800

    style(lab1):add comments in ./src/reassembler.cc
    add comments.

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -6,37 +6,41 @@ using namespace std;
 
 void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring )
 {
-  if(data.empty()){
-    if(is_last_substring){
+  if ( data.empty() ) {
+    if ( is_last_substring ) {
       output_.close();
     }
     return;
   }
+  auto end_index = first_index + data.size();                  // data: [first_index, end_index)
+  auto last_index = next_index_ + output_.available_capacity(); // 可用范围: [next_index_, last_index)
+  if ( end_index < next_index_ || first_index >= last_index ) {
+    return; // 不在可用范围内, 直接返回
+  }
 
-  auto end_index = first_index + data.size();
-  auto last_index = next_index_ + output_.available_capacity();
-  if(end_index < next_index_ || first_index >= last_index) return;
-
-  if(last_index < end_index){
+  // 调整data的范围
+  if ( last_index < end_index ) {
     end_index = last_index;
-    data.resize(end_index - first_index);
+    data.resize( end_index - first_index );
     is_last_substring = false;
   }
-  if(first_index < next_index_){
-    data = data.substr(next_index_ - first_index);
+  if ( first_index < next_index_ ) {
+    data = data.substr( next_index_ - first_index );
     first_index = next_index_;
   }
 
-  if(first_index == next_index_ && (buffer_.empty() || end_index < get<1>(buffer_.front()) + 2)){
-    if(buffer_.size()){
-      data.resize(std::min(end_index, get<0>(buffer_.front())) - first_index);
+  // 若data可以直接写入output, 则直接写入
+  if ( first_index == next_index_ && ( buffer_.empty() || end_index < get<1>( buffer_.front() ) + 2 ) ) {
+    if ( buffer_.size() ) { // 若重叠, 则调整data的范围
+      data.resize( min( end_index, get<0>( buffer_.front() ) ) - first_index );
     }
-    push_to_output(std::move(data), output_);
-  }else{
-    buffer_push(first_index, end_index - 1, data);
+    push_to_output( move( data ), output_ );
+  } else { // 否则, 将data插入buffer_
+    buffer_push( first_index, end_index - 1, data );
   }
-
   had_last_ |= is_last_substring;
+  
+  // 尝试将buffer_中的数据写入output
   buffer_pop(output_);
 }
 
@@ -51,40 +55,44 @@ void Reassembler::push_to_output(std::string data, ByteStream& output){
 }
 
 void Reassembler::buffer_push(uint64_t first_index, uint64_t last_index, std::string data){
-  auto left = first_index, right = last_index;
-  auto begin = buffer_.begin(), end = buffer_.end();
-  auto lef = lower_bound(begin, end, left, [](auto& a, auto& b) {return get<1>(a) < b; });
-  auto rig = upper_bound(lef, end, right, [](auto& b, auto& a) {return get<0>(a) > b; });
-  if(lef != end) left = std::min(left, get<0>(*lef));
-  if(rig != begin) right = std::max(right, get<1>(*prev(rig)));
-
-  if(lef != end && get<0>(*lef) == left && get<1>(*lef) == right) return;
-
-  buffer_size_ += 1 + right - left;
-  if(data.size() == right - left + 1 && lef == rig){
-    buffer_.emplace(rig, left, right, move(data));
+  // 合并区间
+  auto l = first_index, r = last_index;
+  auto beg = buffer_.begin(), end = buffer_.end();
+  auto lef = lower_bound( beg, end, l, []( auto& a, auto& b ) { return get<1>( a ) < b; } );
+  auto rig = upper_bound( lef, end, r, []( auto& b, auto& a ) { return get<0>( a ) > b; } );
+  if (lef != end) l = min( l, get<0>( *lef ) );
+  if (rig != beg) r = max( r, get<1>( *prev( rig ) ) );
+  
+  // 当data已在buffer_中时，直接返回
+  if ( lef != end && get<0>( *lef ) == l && get<1>( *lef ) == r ) {
     return;
   }
-  std::string s(1 + right - left, 0);
 
-  for(auto&& it : views::iota(lef, rig)){
-    auto& [a, b, c] = *it;
-    buffer_size_ -= c.size();
-    ranges::copy(c, s.begin() + a - left);
+  buffer_size_ += 1 + r - l;
+  if ( data.size() == r - l + 1 && lef == rig ) { // 当buffer_中没有data重叠的部分
+	buffer_.emplace( rig, l, r, move( data ) );
+	return;
   }
-  ranges::copy(data, s.begin() + first_index - left);
-  buffer_.emplace(buffer_.erase(lef, rig), left, right, std::move(s));
+  string s( 1 + r - l, 0 );
+
+  for ( auto&& it : views::iota( lef, rig ) ) {
+	auto& [a, b, c] = *it;
+	buffer_size_ -= c.size();
+    ranges::copy(c, s.begin() + a - l);
+  }
+  ranges::copy(data, s.begin() + first_index - l);
+  buffer_.emplace( buffer_.erase( lef, rig ), l, r, move( s ) );
 }
 
 void Reassembler::buffer_pop(ByteStream& output){
-  while(!buffer_.empty() && get<0>(buffer_.front()) == next_index_){
+  while ( !buffer_.empty() && get<0>( buffer_.front() ) == next_index_ ) {
     auto& [a, b, c] = buffer_.front();
     buffer_size_ -= c.size();
-    push_to_output(std::move(c), output);
+    push_to_output( move( c ), output ); 
     buffer_.pop_front();
   }
 
-  if(had_last_ && buffer_.empty()){
+  if ( had_last_ && buffer_.empty() ) {
     output.close();
   }
 }
